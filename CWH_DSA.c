c62c89a59d48d62400bc351bdfd1b1af491ca306
Rohit Goswami <rohitgoswami@Rohits-MacBook-Air.local>
2025-10-21 14:16:34 +0530

    Insertion LinkedList prac

--- CWH_DSA.c
+++ CWH_DSA.c
@@ -57,45 +57,154 @@
 //     return -1;
 // };
 
-int BinarySearch(int arr[], int size, int element)
-{
-    int low, mid, high;
-    low = 0;
-    high = size-1;
+// int BinarySearch(int arr[], int size, int element)
+// {
+//     int low, mid, high;
+//     low = 0;
+//     high = size-1;
 
-    // start searching
-    while (low <= high)
+//     // start searching
+//     while (low <= high)
+//     {
+//         mid = (low + high) / 2; // in case of decimal, the integer value will be auto printed by default
+//         if (arr[mid] == element)
+//         {
+//             return mid;
+//         }
+//         else if (arr[mid] < element)
+//         {
+//             low = mid + 1;
+//         }
+//         else if (arr[mid] > element)
+//         {
+//             high = mid - 1;
+//         }
+//         // searching ends
+//     };
+//     return -1;
+// };
+
+// int main()
+// {
+//     // Unsorted arr for Linear Search
+//     // int arr[] = {1, 2, 3, 34, 215, 16, 72, 18, 9, 10, 23, 889, 908, 113, 21, 20, 55, 2, 98, 34, 5, 6, 7};
+//     // int arrSize = sizeof(arr) / sizeof(int);
+
+//     // Sorted arr for Binary Search
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+//     int arrSize = sizeof(arr) / sizeof(int);
+//     int element = 15;
+//     int searchIndex = BinarySearch(arr, arrSize, element);
+//     printf("The element %d was found @ index %d\n", element, searchIndex);
+
+//     return 0;
+// };
+
+// Linked Lists
+
+typedef struct Node
+{
+    int data;
+    struct Node *next; // self-referencing pointer
+} Node;
+
+struct Node *head;
+struct Node *second;
+struct Node *third;
+
+void CreateLinkedList(){
+    head = (Node *)malloc(sizeof(Node));
+    second = (Node *)malloc(sizeof(Node));
+    third = (Node *)malloc(sizeof(Node));
+
+    // first & second node linked
+    head->data = 7;
+    head->next = second;
+
+    // second & third node linked
+    second->data = 14;
+    second->next = third;
+
+    // third node points to null
+    third->data = 13;
+    third->next = NULL; // terminate Linked list at third node
+};
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
     {
-        mid = (low + high) / 2; // in case of decimal, the integer value will be auto printed by default
-        if (arr[mid] == element)
-        {
-            return mid;
-        }
-        else if (arr[mid] < element)
-        {
-            low = mid + 1;
-        }
-        else if (arr[mid] > element)
-        {
-            high = mid - 1;
-        }
-        // searching ends
-    };
-    return -1;
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+
+};
+
+// Inserting data at head 
+struct Node*  InsertDataAtHead(struct Node *head, int data){
+    struct Node* ptr = (Node*)malloc(sizeof(Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr;
+};
+
+// Inserting data in middle randomly 
+struct Node*  InsertDataInMiddle(struct Node *head, int data, int index){
+    struct Node* ptr = (Node*)malloc(sizeof(Node));
+    struct Node* p = head;
+    int i = 0;
+    while (i != index-1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+};
+
+// Inserting data at specific node
+struct Node*  InsertDataAtSpecificNode(struct Node *head, struct Node *prevNode, int data){
+    // A pointer will be given in this case pointing to a node
+    struct Node* ptr = (Node*)malloc(sizeof(Node));
+    ptr->data = data;
+
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+
+
+    return head;
+};
+
+// Inserting data at end
+struct Node*  InsertDataAtEnd(struct Node *head, int data){
+    struct Node* ptr = (Node*)malloc(sizeof(Node));
+    ptr->data = data;
+    struct Node* p = head;
+
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
 };
 
 int main()
 {
-    // Unsorted arr for Linear Search
-    // int arr[] = {1, 2, 3, 34, 215, 16, 72, 18, 9, 10, 23, 889, 908, 113, 21, 20, 55, 2, 98, 34, 5, 6, 7};
-    // int arrSize = sizeof(arr) / sizeof(int);
+    // Linked List creation
+    CreateLinkedList();
 
-    // Sorted arr for Binary Search
-    int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int arrSize = sizeof(arr) / sizeof(int);
-    int element = 15;
-    int searchIndex = BinarySearch(arr, arrSize, element);
-    printf("The element %d was found @ index %d\n", element, searchIndex);
+    //Inserting Node
+    //head = InsertDataAtHead(head, 71);
+    //head = InsertDataInMiddle(head, 25, 3);
+    //head = InsertDataAtSpecificNode(head, second, 2);
+    //head = InsertDataAtEnd(head, 62);
+
+    // printing the list
+    LinkedListTraversal(head);
 
     return 0;
 };
